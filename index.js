537c8785f739f8bae015b939c2de52884e999b9c
notobd <notobd4x4@gmail.com>
2024-11-02 11:19:16 -0700

    more changes

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const profileDiv = document.querySelector('.profile');
 console.log(profileDiv);
 const profileInfo = document.querySelector('.profile-info');
 console.log(profileInfo);
+const last = document.querySelector('.mobsoc');
+console.log(last);
 // const sec = document.querySelector('.hide');
 // console.log(sec);
 
@@ -17,17 +19,27 @@ const html = `<p>SHARE</p>
  <img src="/images/icon-pinterest.svg" alt="pinterest">
  <img src="/images/icon-twitter.svg" alt="x">`;
 
-const html2 = `<p>SHARE</p>
+const html2 = `<div class="js-div">
+<div class="nest-div">
+<p>SHARE</p>
  <img src="/images/icon-facebook.svg" alt="facebook">
  <img src="/images/icon-pinterest.svg" alt="pinterest">
- <img src="/images/icon-twitter.svg" alt="x">
-`;
+ <img src="/images/icon-twitter.svg" alt="x" >
+ </div>
+ <div class="js-divv">
+<img src="/images/icon-share.svg" alt="ahre icon" class="sharerr" />
+ </div>
+ </div>`;
+
+//  <button type="button" class="button"> </button>
 
 const shareBtn = () => {
   if (window.innerWidth < 768) {
     // profileInfo.classList.toggle('hidden');
-    profileInfo.innerHTML = html2;
-    // profileDiv.classList.toggle('hidden');
+    last.innerHTML = html2;
+    // btn.classList.add('hidden');
+    last.classList.remove('hidden');
+    profileDiv.classList.add('hidden');
   } else {
     socials.innerHTML = html;
     display.classList.toggle('hidden');
