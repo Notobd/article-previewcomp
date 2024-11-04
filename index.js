c5135f15e20bbab7712c3f2ac2a21fd81a682499
notobd <notobd4x4@gmail.com>
2024-11-04 08:56:41 -0800

    more updates

--- index.js
+++ index.js
@@ -11,39 +11,41 @@ const profileInfo = document.querySelector('.profile-info');
 console.log(profileInfo);
 const last = document.querySelector('.mobsoc');
 console.log(last);
-// const sec = document.querySelector('.hide');
-// console.log(sec);
 
+//created html for the wider screens sections socials using js and using innerHTML to replace the targeted div when the button is clicked
 const html = `<p>SHARE</p>
  <img src="/images/icon-facebook.svg" alt="facebook" class ="face">
  <img src="/images/icon-pinterest.svg" alt="pinterest">
  <img src="/images/icon-twitter.svg" alt="x">`;
 
+//created html for the mobile view socials sections using js and using innerHTML to replace the targeted div when the button is clicked
 const html2 = `<div class="js-div">
 <div class="nest-div">
 <p>SHARE</p>
  <img src="/images/icon-facebook.svg" alt="facebook">
- <img src="/images/icon-pinterest.svg" alt="pinterest">
  <img src="/images/icon-twitter.svg" alt="x" >
+  <img src="/images/icon-pinterest.svg" alt="pinterest">
  </div>
  <div class="js-divv">
 <img src="/images/icon-share.svg" alt="ahre icon" class="sharerr" />
  </div>
  </div>`;
 
-//  <button type="button" class="button"> </button>
-
+//shareBtn function here handles functionality and using window.innerWidth the display changes on various screensizes
 const shareBtn = () => {
   if (window.innerWidth < 768) {
-    // profileInfo.classList.toggle('hidden');
     last.innerHTML = html2;
-    // btn.classList.add('hidden');
     last.classList.remove('hidden');
     profileDiv.classList.add('hidden');
+    display.classList.toggle('hidden');
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1025) {
+    socials.innerHTML = html;
+    display.classList.toggle('hidden');
   } else {
     socials.innerHTML = html;
     display.classList.toggle('hidden');
   }
 };
 
+//and finally event handler higher order function here accepst a callback function that our functionality is encpsulated in and works when the share button is pressed
 btn.addEventListener('click', shareBtn);
